0867bca6193d2f2e4c969db84ed6639828b85b29
Will Spaeth <jwspaeth0@gmail.com>
2018-10-09 10:45:43 -0500

    Updating changes

--- TD_algorithm.py
+++ TD_algorithm.py
@@ -6,7 +6,7 @@
 # •Complete weight update method
 # •Complete train method
 # •Compile statistics needed for analysis
-
+# •Redo reward function
 
 # Raw state space: [cos(theta1) sin(theta1) cos(theta2) sin(theta2) dottheta1 dottheta2]
 #     •Bounds: -1 <= cos or sin(theta1 or theta2) <= 1; -4pi <= dottheta1 <= 4pi; -9pi <= dottheta2 <= 9pi
@@ -40,7 +40,7 @@ def tiling(state_list, tiles, tile_offsets, tile_widths):
 
     processed_states = process_states(state_list)
 
-    print(processed_states)
+    #print(processed_states)
     counts = np.zeros((4,4), dtype=np.int32)
     for i in range(4):
         benchmark = np.zeros((4), dtype=np.float32)
@@ -74,6 +74,8 @@ def tiling(state_list, tiles, tile_offsets, tile_widths):
 # Feed this function raw state list
 def process_states(state_list):
 
+    #print(state_list)
+
     processed_states = 4*[0]
     processed_states[0] = np.arctan2(state_list[1], state_list[0])
     processed_states[1] = np.arctan2(state_list[3], state_list[2])
@@ -98,71 +100,109 @@ def calculate_reward(state_list):
 
 def calculate_state_value(features, weights):
     weighted_mult = np.multiply(features, weights)
-    state_value = np.reduce_sum(weighted_mult)
+    state_value = np.sum(weighted_mult)
     
     return state_value
 
 # Learning algorithm to implemented: TD, Sarsa, Expected Sarsa
-def weight_update(alpha, gamma, reward, 
+def weight_update(alpha, lamb, gamma, z, reward, 
                   features, weights, state_value, state_value_prime):
 
-    weights = weights + alpha*(reward + (gamma*state_value_prime) - state_value)*features
+    error = reward + gamma*state_value_prime - state_value
 
-    return weights
+    z = gamma*lamb*z+features
+
+    weights = weights + alpha*error*z
+
+    return weights, z
     
 
 # Policy choice
 def policy_choice(action_space, env, weights, tiles, tile_offsets, tile_widths, epsilon):
 
+    action = 0
+
     max_value = 0
     max_value_index = 0
     for i in range(action_space.shape[0]):
+#        env.close()
         dummy_env = copy.deepcopy(env)
-        raw_state = dummy_env.step(action_space[i])
+        raw_state, _, _, _ = dummy_env.step(action_space[i])
         features = tiling(raw_state, tiles, tile_offsets, tile_widths)
         value = calculate_state_value(features, weights)
         if i == 0:
             max_value = value
             max_value_index = 0
-        else if value > max_value:
+        elif value > max_value:
             max_value = value
             max_value_index = i
+        #print("Value:", value, "Index:", i, "Action:", action_space[i])
+        processed_state = process_states(raw_state)
+        #print("The state:", processed_state)
 
     # Do actual policy part here
+    flip = np.random.binomial(size=1, n=1, p=epsilon)
+    if flip == 1:
+        #print("Max chosen")
+        return action_space[max_value_index]
+    else:
+        #print("Explore chosen")
+        action_index = np.random.randint(action_space.shape[0])
+        while action_index == max_value_index:
+            action_index = np.random.randint(action_space.shape[0])
+            
+        return action_space[action_index]
 
 
 # Training
 def train(tiles, tile_offsets, tile_widths,
-          alpha, gamma, weights, epsilon, n_episodes, n_timesteps):
+          alpha, gamma, lamb, weights, epsilon, n_episodes, n_timesteps):
     
     env = gym.make("Acrobot-v1")
-    n_episodes = 0
     action_space = np.asarray((-1,0,1), dtype=np.int32)
     training_rewards = np.zeros((n_episodes), dtype=np.float32)
 
+    print("HERE WE GOOOOO!!!")
+    print(n_episodes)
 
     for i in range(n_episodes):
-                
+
         env.reset()
         total_rewards = 0
 
-        for j in range(n_timesteps):
-            action = policy_choice(env, epsilon) # Get first action
-            raw_state = env.step(action) # Get first state
-            features = tiling(raw_state, tile, tile_offsets, tile_widths, weights) # Get first features
+        z = 0
+
+        timestep_count = 0
+        terminal = False
+        while terminal is False:
+
+            action = policy_choice(action_space, env, weights, tiles, tile_offsets, tile_widths, epsilon) # Get first action
+            #print("First action:", action)
+
+            # TD Prediction: starting in raw state and following vpi thereafter
+ #           env.render()
+            raw_state, _, _, _ = env.step(action) # Get first state
+            features = tiling(raw_state, tiles, tile_offsets, tile_widths) # Get first features
             state_value = calculate_state_value(features, weights) # V(s)
-            action = policy_choice(env, epsilon) # Get second action
-            raw_state_prime = env.step(action) # Get second state
-            reward = calculate_reward(raw_state_prime) # Get Rt+1
-            features_prime = tiling(raw_state_prime, tile, tile_offsets, tile_widths, weights) # Get second features
+            action = policy_choice(action_space, env, weights, tiles, tile_offsets, tile_widths, epsilon) # Get second action
+            #print("Second action:", action)
+            raw_state_prime, reward, terminal, _ = env.step(action) # Get second state, Rt+1
+            ### reward = calculate_reward(raw_state_prime) # Get Rt+1
+            features_prime = tiling(raw_state_prime, tiles, tile_offsets, tile_widths) # Get second features
             state_value_prime = calculate_state_value(features, weights) # V(s')
 
-            weights = weight_update(alpha, gamma, reward, 
-                                    features, weights, state_value, state_value_prime) # Update weights to learn
+            weights, z = weight_update(alpha=alpha, lamb=lamb, gamma=gamma, reward=reward, z=z,
+                                    features=features, weights=weights, state_value=state_value, state_value_prime=state_value_prime) # Update weights to learn
             total_rewards += reward
 
+            print("\tTimestep:", timestep_count, "Terminal:", terminal)
+            timestep_count += 1
+
         training_rewards[i] = total_rewards
-            
+        
+        print("Episode:", i)
+        print("Reward:", total_rewards)
+        print()
 
     return weights, training_rewards
 
@@ -182,10 +222,23 @@ def train(tiles, tile_offsets, tile_widths,
 ###
 # 1) Initialize feature vector and weight vector
 
+# Originally
+'''
 dim1 = (2.2*math.pi/12)/4
 dim2 = dim1
 dim3 = (8.8*math.pi/48)/4
 dim4 = (19.8*math.pi/104)/4
+'''
+
+divisor1 = 24
+divisor2 = 24
+divisor3 = 48
+divisor4 = 104
+
+dim1 = (2.2*math.pi/divisor1)/4
+dim2 = dim1
+dim3 = (8.8*math.pi/divisor3)/4
+dim4 = (19.8*math.pi/divisor4)/4
 
 unit_vector = np.asarray((dim1,dim2,dim3,dim4), dtype=np.float32)
 
@@ -204,9 +257,9 @@ tile_offsets[3] = tile_offsets[3]*unit_vector*-1+tile_offsets[0]
 for i in range(4):
     print(tile_offsets[i])
 
-tiles = np.empty((4, 12, 12, 48, 104), dtype=np.int32)
-tile_widths = [(2.2*math.pi)/12, (2.2*math.pi)/12, (8.8*math.pi)/48, (19.8*math.pi)/104]
-weights = np.zeros((4,12, 12, 48, 104), dtype=np.float32)
+tiles = np.empty((4, divisor1, divisor2, divisor3, divisor4), dtype=np.int32)
+tile_widths = [(2.2*math.pi)/divisor1, (2.2*math.pi)/divisor2, (8.8*math.pi)/divisor3, (19.8*math.pi)/divisor4]
+weights = np.zeros((4, divisor1, divisor2, divisor3, divisor4), dtype=np.float32)
 
 ###
 # 2) Train
@@ -220,3 +273,8 @@ weights = np.zeros((4,12, 12, 48, 104), dtype=np.float32)
 env = gym.make("Acrobot-v1")                                                                                                                                                      
 env.reset()     
 
+post_weights, post_rewards = train(tiles=tiles, tile_offsets=tile_offsets, tile_widths=tile_widths,
+      alpha=1/4, lamb=.8, gamma=.9, weights=weights, epsilon=.9, n_episodes=10, n_timesteps=500)
+
+print(post_weights.shape)
+print(post_rewards.shape)
