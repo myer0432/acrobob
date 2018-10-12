d6dbd9ab12a9e105f65520e31032e65275e0261c
Will Spaeth <jwspaeth0@gmail.com>
2018-10-12 15:46:09 -0500

    Adding what I've done since Wednesday

--- Double_Sarsa_algorithm.py
+++ Double_Sarsa_algorithm.py
@@ -5,45 +5,18 @@
 # Processed state space: [theta1 theta2 dottheta1 dottheta2]
 # Raw action space: [-1, 0, +1] for torque of middle link
 
+### TODO:
+# 1) Modify tiling to support actions as well as states
+# 2) Modify weight calculations to happen for all tilings
+# 3) Implement Double Sarsa
+
 import gym
 import numpy as np
 import math
 import copy
 
-def tiling(state_list, tiles, tile_offsets, tile_widths):
-
-    processed_states = process_states(state_list)
-
-    counts = np.zeros( tiles.shape, dtype=np.int32)
-
-    for i in range(4):
-        benchmark = np.zeros((4), dtype=np.float32)
-        benchmark_high = np.zeros((4), dtype=np.float32)
-
-        benchmark[0] = tile_offsets[i][0]+tile_widths[0]
-        while processed_states[0] > benchmark[0] and counts[i,0] < 11:
-            benchmark[0] += tile_widths[0]
-            counts[i,0] += 1   
-
-        benchmark[1] = tile_offsets[i][1]+tile_widths[1]
-        while processed_states[1] > benchmark[1] and counts[i,1] < 11:
-            benchmark[1] += tile_widths[1]
-            counts[i,1] += 1
-                
-        benchmark[2] = tile_offsets[i][2]+tile_widths[2]
-        while processed_states[2] > benchmark[2] and counts[i,2] < 47:
-            benchmark[2] += tile_widths[2]
-            counts[i,2] += 1
-                    
-        benchmark[3] = tile_offsets[i][3]+tile_widths[3]
-        while processed_states[3] > benchmark[3] and counts[i,3] < 107:
-            benchmark[3] += tile_widths[3]
-            counts[i,3] += 1
-            
-        tiles[i, counts[i,0], counts[i,1], counts[i,2], counts[i,3]] = 1
-        features = tiles
-
-        return features
+def tiling():
+    a = 0
 
 # Feed this function raw state list
 def process_states(state_list):
@@ -58,80 +31,21 @@ def process_states(state_list):
 
     return processed_states
 
-def calculate_state_action_value(features, weights):
-    weighted_mult = np.multiply(features, weights)
-    state_value = np.sum(weighted_mult)
-    
-    return state_value
+def calculate_state_action_value():
+    a = 0
 
 # Learning algorithm to implemented: TD, Sarsa, Expected Sarsa
-def weight_update(alpha, lamb, gamma, z, reward, 
-                  features, weights, state_value, state_value_prime):
-
-    error = reward + gamma*state_value_prime - state_value
-
-    z = gamma*lamb*z+features
-
-    weights = weights + alpha*error*z
-
-    return weights, z
+def weight_update():
+    a = 0
     
 
 # Policy choice
-def policy_choice(action_space, env, weights, tiles, tile_offsets, tile_widths, epsilon):
+def policy_choice():
     a = 0
 
 # Training
-def train(tiles, tile_offsets, tile_widths,
-          alpha, gamma, lamb, weights, epsilon, n_episodes, n_timesteps):
-    
-    env = gym.make("Acrobot-v1")
-    action_space = np.asarray((-1,0,1), dtype=np.int32)
-    training_rewards = np.zeros((n_episodes), dtype=np.float32)
-
-    print("HERE WE GOOOOO!!!")
-    print(n_episodes)
-
-    for i in range(n_episodes):
-
-        env.reset()
-        total_rewards = 0
-
-        z = 0
-
-        timestep_count = 0
-        terminal = False
-        while terminal is False:
-
-            action = policy_choice(action_space, env, weights, tiles, tile_offsets, tile_widths, epsilon) # Get first action
-            #print("First action:", action)
-
-            # TD Prediction: starting in raw state and following vpi thereafter
- #           env.render()
-            raw_state, _, _, _ = env.step(action) # Get first state
-            features = tiling(raw_state, tiles, tile_offsets, tile_widths) # Get first features
-            state_value = calculate_state_value(features, weights) # V(s)
-            action = policy_choice(action_space, env, weights, tiles, tile_offsets, tile_widths, epsilon) # Get second action
-            #print("Second action:", action)
-            raw_state_prime, reward, terminal, _ = env.step(action) # Get second state, Rt+1
-            ### reward = calculate_reward(raw_state_prime) # Get Rt+1
-            features_prime = tiling(raw_state_prime, tiles, tile_offsets, tile_widths) # Get second features
-            state_value_prime = calculate_state_value(features, weights) # V(s')
-
-            weights, z = weight_update(alpha=alpha, lamb=lamb, gamma=gamma, reward=reward, z=z,
-                                    features=features, weights=weights, state_value=state_value, state_value_prime=state_value_prime) # Update weights to learn
-            total_rewards += reward
-
-            print("\tTimestep:", timestep_count, "Terminal:", terminal)
-            timestep_count += 1
-
-        training_rewards[i] = total_rewards
-        
-        print("Episode:", i)
-        print("Reward:", total_rewards)
-        print()
-
-    return weights, training_rewards
+def train():
+    a = 0
 
 ################
 # Program Plan #
@@ -157,7 +71,7 @@ def train(tiles, tile_offsets, tile_widths,
 
 ### Create offset buckets
 # 1 set, 12 tilings, 4 dimensions
-offsets_1 = np.zeros((12,4), dtype=np.float32)
+offsets_1 = np.zeros((1,12,4), dtype=np.float32)
 
 # 4 sets, 3 tilings, 3 dimensions
 offsets_2 = np.zeros((4,3,3), dtype=np.float32)
@@ -166,71 +80,80 @@ offsets_2 = np.zeros((4,3,3), dtype=np.float32)
 offsets_3 = np.zeros((6,2,2), dtype=np.float32)
 
 # 4 sets, 3 tilings, 1 dimension
-offsets_4 = np.zeros((4,3), dtype=np.float32)
+offsets_4 = np.zeros((4,3,1), dtype=np.float32)
 
-offset_list = [offsets_1, offsets_2, offsets_3, offsets_4]
+offset_list = np.asarray((offsets_1, offsets_2, offsets_3, offsets_4), dtype=object)
 
 ### Create dimension bucket options
-offset_options = np.asarray((2.2*math.pi/6, 2.2*math.pi/6, 8.8*math.pi/7, 19.8*math.pi/7), dtype=np.float32)
+interval_options = np.asarray((2.2*math.pi/6, 2.2*math.pi/6, 8.8*math.pi/7, 19.8*math.pi/7), dtype=np.float32)
 origin_options = np.asarray((-1.05*math.pi, -1.05*math.pi, -4.4*math.pi, -9.9*math.pi), dtype=np.float32)
 
 # First set of tilings
-set_1 = np.asarray((0,1,2,3), dtype=np.float32)
+set_1 = np.asarray((0,1,2,3), dtype=np.int32)
+set_1 = np.expand_dims(set_1, axis=0)
 
 # Second set of tilings
-set_2 = np.asarray( ( (0,1,2), (0,1,3), (0,2,3), (1,2,3) ), dtype=np.float32)
+set_2 = np.asarray( ( (0,1,2), (0,1,3), (0,2,3), (1,2,3) ), dtype=np.int32)
 
 # Third set of tilings
-set_3 = np.asarray( ( (0,1), (0,2), (0,3), (1,2), (1,3), (2,3) ), dtype=np.float32)
+set_3 = np.asarray( ( (0,1), (0,2), (0,3), (1,2), (1,3), (2,3) ), dtype=np.int32)
 
 # Fourth set of tilings
-set_4 = np.asarray( ( (0), (1), (2), (3) ), dtype=np.float32)
+set_4 = np.asarray( ( (0), (1), (2), (3) ), dtype=np.int32)
+set_4 = np.expand_dims(set_4, axis=1)
 
-set_list = [set_1, set_2, set_3, set_4]
+set_list = np.asarray((set_1, set_2, set_3, set_4), dtype=object)
 
 ### Initialize offsets
 for i in range(4):
-    print(offset_list[i].shape)
-    print(set_list[i].shape)
+    print("Offset:", offset_list[i].shape)
+    print("Count:", counts[i].shape)
+    print("Dimension order:", set_list[i].shape)
     print()
 
-for i in range(12):
-    for j in range(4):
-
-        origin = origin_options[ set_1[j] ]
-        base_interval = offset_options[ set_1[j] ]
-        actual_offset = -1*np.random.uniform(high=base_interval)
-
-        offsets_1[i,j] = actual_offset
-
+# 4 types of tilings, 12 for each type
 for i in range(4):
-    for j in range(3):
-        for k in range(3):
+
+    # Number of sets
+    for j in range( offset_list[i].shape[0] ):
+        
+        # Number of tilings
+        for k in range( offset_list[i].shape[1] ):
             
-            origin = origin_options[ set_2[i,k] ]
-            base_interval = offset_options[ set_2[i,k ] ]
-            actual_offset = -1*np.random.uniform(high=base_interval)
+            # Number of dimensions
+            for m in range( offset_list[i].shape[2] ):
+                
+                origin = origin_options[ set_list[i][j,m] ]
+                base_interval = interval_options[ set_list[i][j,m] ]
+                actual_offset = -1*np.random.uniform(high=base_interval)
 
-            offsets_2[i,j,k] = actual_offset
+                offset_list[i][j,k,m] = actual_offset + origin
 
-for i in range(6):
-    for j in range(2):
-        for k in range(2):
+                print(origin, base_interval, actual_offset, offset_list[i][j,k,m])
 
-            origin = origin_options[ set_3[i,k] ]
-            base_interval = offset_options[ set_3[i,k] ]
-            actual_offset = -1*np.random.uniform(high=base_interval)
+np.save(
 
-            offsets_3[i,j,k] = actual_offset
 
-for i in range(4):
-    for j in range(3):
+def tiling(state_list, set_list, offset_list, interval_options):
 
-        origin = origin_options[ set_4[i] ]
-        base_interval = offset_options[ set_4[i] ]
-        actual_offset = -1*np.random.uniform(high=base_interval)
+    counts = np.empty(offset_list.shape[0], dtype=object)
+    for i in range(counts.shape[0]):
+        counts[i] = np.zeros(offset_list[i].shape, dtype=np.int32)
 
-        offsets_4[i,j] = actual_offset
+    processed_states = process_states(state_list)
 
-def tiling(state_list, ):
-    a = 0
+    for i in range( offset_list.shape[0] ):
+        
+        for j in range( offset_list[i].shape[0] ):
+            
+            for k in range( offset_list[i].shape[1] ):
+                
+                for m in range( offset_list[i].shape[2] ):
+                    
+                    base_interval = interval_options[ set_list[i][j,m] ]
+                    cursor = offset_list[i][j,k,m] + base_interval
+                    while( (count[i][j,k,m] < (set_list[i][j,m]-1)) && (processed_states > cursor) ):
+                        counts[i][j,k,m] += 1
+                        cursor += base_interval
+                  
+    return 0
